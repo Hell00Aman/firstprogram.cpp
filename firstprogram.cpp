1292e1fa3bcf14e8f039b5280b4d6515309bd2c8
AMAN RAJ <amantk2205@gmail.com>
2025-09-16 22:09:12 +0530

    Add files via upload

---
+++ firstprogram.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>  // Includes all standard C++ libraries
+using namespace std;     // Allows use of standard names without std::
+
+int main() {
+    cout << "Hello";     // Prints "Hello" on the screen
+    return 0;            // Ends program successfully
+}
